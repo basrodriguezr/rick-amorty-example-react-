232ecc810c4126d1f8d129ffb120a606774cbddb
bastian rodríguez <basrodriguezr@gmail.com>
2022-07-21 19:33:16 -0400

    creando rutas y limpiando proyecto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Inicio from './components/Inicio';
+import Personaje from './components/Personaje';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Router>
+            <Route path="/" element={<Inicio></Inicio>}></Route>
+            <Route path="/personaje:id" element={<Personaje></Personaje>}></Route>
+        </Router>
     </div>
   );
 }
